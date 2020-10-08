05db6c8c761990787e0d8c2f0e4916c5082e16e5
cat - lin <plummorgan@gmail.com>
2020-10-08 14:18:39 -0700

    testing player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,13 +3,18 @@ const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
+console.log('is this the', new Potion());
+
 
 test('creates a player object', () => {
+
     const player = new Player('Dave');
 
     expect(player.name).toBe('Dave');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    // expect(player.inventory).toEqual(
+    //     expect.arrayContaining([expect.any(Object)])
+    // );
 });
